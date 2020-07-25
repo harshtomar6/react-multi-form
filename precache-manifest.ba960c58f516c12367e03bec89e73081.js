718611f517d1884460ce4b721662eeb66d86e389
Harsh Tomar <harsh@zoop.one>
2020-07-25 16:33:21 +0530

    Updates

---
+++ precache-manifest.ba960c58f516c12367e03bec89e73081.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e86e0cc26399ebf9054a9b0d6c260747",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7a84e5fce612c02e2679",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "2efdb1ba53255baa76ff",
+    "url": "./static/js/2.31ed4733.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.31ed4733.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a84e5fce612c02e2679",
+    "url": "./static/js/main.9089e8fa.chunk.js"
+  },
+  {
+    "revision": "7588f0c8e9b9a140613d",
+    "url": "./static/js/runtime-main.9b0e9a66.js"
+  }
+]);
